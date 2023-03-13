a30786d2b96748eed085e5fbccd160f457d6f3a3
YusProduction <yusraasimsyseng@gmail.com>
2023-03-13 16:08:37 +0500

    Web page

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:test_web/pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
